363c5642c9fd549bfb7be7d6df35b225fc1b86b1
niru1990 <nicolas.reyes90@gmail.com>
2020-05-15 16:42:20 -0300

    Se agregan clase y layout para historia clinica del modulo de pacientes

---
+++ app/src/main/java/com/example/donar/pacientesHistoriaCLinica.java
@@ -0,0 +1,14 @@
+package com.example.donar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pacientesHistoriaCLinica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pacientes_historia_c_linica);
+    }
+}
