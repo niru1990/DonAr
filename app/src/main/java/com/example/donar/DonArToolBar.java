6047e340f7e2851e03e8196beb08049c3e2f1e39
Daniel Guzman <daniel.guzman@ort.edu.ar>
2020-05-22 01:35:08 -0300

    Elementos clickeables dentro del toolbar

--- app/src/main/java/com/example/donar/DonArToolBar.java
+++ app/src/main/java/com/example/donar/DonArToolBar.java
@@ -1,16 +1,20 @@
 package com.example.donar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+
 import org.jetbrains.annotations.NotNull;
 
-public class DonArToolBar extends AppCompatActivity implements IToolBar, View.OnClickListener{
+public class DonArToolBar extends AppCompatActivity implements IToolBar{
 
     public TextView titulo;
     public TextView nombreUsuario;
@@ -23,6 +27,11 @@ public class DonArToolBar extends AppCompatActivity implements IToolBar, View.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_don_ar_tool_bar);
 
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.donArToolBar);
+        setSupportActionBar(myToolbar);
+
+        /**
         titulo = (TextView) findViewById(R.id.txtAplicacionYModulo);
         nombreUsuario = (TextView) findViewById(R.id.txtUsuario);
         idUser = (TextView) findViewById(R.id.txtId);
@@ -30,18 +39,36 @@ public class DonArToolBar extends AppCompatActivity implements IToolBar, View.On
         registrar = (Button) findViewById(R.id.btnRegistro);
         login = (Button) findViewById(R.id.btnLogin);
 
+        **/
+
+        /*
         registrar.setOnClickListener(this);
         login.setOnClickListener(this);
 
+         */
+
+    }
+
+    /**
+     * Aqui creamos las opciones de Menu con el toolbar.
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 
     @SuppressLint("SetTextI18n")
     @Override
     public void dataChangeToolbar(String textoTitulo, String usuario, String idUsuario, boolean verBotones) {
+        /**
         titulo = (TextView) findViewById(R.id.txtAplicacionYModulo);
         nombreUsuario = (TextView) findViewById(R.id.txtUsuario);
         idUser = (TextView) findViewById(R.id.txtId);
-
+        **/
         titulo.setText("DonAr - " + textoTitulo);
         nombreUsuario.setText(usuario);
         idUser.setText(idUsuario);
@@ -51,8 +78,11 @@ public class DonArToolBar extends AppCompatActivity implements IToolBar, View.On
 
     @Override
     public void mostrarBotones(boolean mostrar) {
+
+        /**
         registrar = (Button) findViewById(R.id.btnRegistro);
         login = (Button) findViewById(R.id.btnLogin);
+         */
         if(mostrar)
         {
             registrar.setVisibility(View.VISIBLE);
@@ -62,23 +92,6 @@ public class DonArToolBar extends AppCompatActivity implements IToolBar, View.On
         {
             registrar.setVisibility(View.INVISIBLE);
             login.setVisibility(View.INVISIBLE);
-        }
-    }
-
-
-    @Override
-    public void onClick(@NotNull View v) {
-        String s;
-        switch (v.getId())
-        {
-            case R.id.btnRegistro:
-                s = "Intento ingresar";
-                break;
-            case R.id.btnLogin:
-                s= "Ingreso";
-                break;
-            default:
-                break;
         }
     }
 }
--- app/src/main/java/com/example/donar/MainActivity.java
+++ app/src/main/java/com/example/donar/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.donar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -8,11 +9,16 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends DonArToolBar implements View.OnClickListener {
+//public class MainActivity extends DonArToolBar implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     private ImageButton donaciones;
     private ImageButton voluntarios;
@@ -34,16 +40,16 @@ public class MainActivity extends DonArToolBar implements View.OnClickListener {
         voluntarios = (ImageButton) findViewById(R.id.imbVoluntarios);
         pacientes = (ImageButton) findViewById(R.id.imbPacientes);
         reportes = (ImageButton) findViewById(R.id.imbReportes);
-        id = (TextView)findViewById(R.id.txtId);
-        usuario = (TextView) findViewById(R.id.txtUsuario);
+        //id = (TextView)findViewById(R.id.txtId);
+        //usuario = (TextView) findViewById(R.id.txtUsuario);
 
         donaciones.setOnClickListener(this);
         voluntarios.setOnClickListener(this);
         pacientes.setOnClickListener(this);
         reportes.setOnClickListener(this);
 
-        active = (verificarConexion() && id.getText().toString().compareTo(" ") != 0);
-        //active = true; //SOLO PRUEBA
+        //active = (verificarConexion() && id.getText().toString().compareTo(" ") != 0);
+        active = true; //SOLO PRUEBA
 
         if (active) {
             donaciones.setImageResource(R.mipmap.boton_donaciones);
@@ -58,15 +64,70 @@ public class MainActivity extends DonArToolBar implements View.OnClickListener {
             reportes.setImageResource(R.mipmap.boton_reportes_gris);
         }
 
+        /*
         dataChangeToolbar("Pacientes -  Consulta", usuario.getText().toString(),
                 id.getText().toString(), false);
+
+         */
+
+        /**
+         * Aqui agrego los datos del toolbar
+         */
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.donArToolBar);
+        setSupportActionBar(myToolbar);
+
     }
 
+    /**
+     * Aqui creamos las opciones de Menu con el toolbar.
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    /**
+     * Aqui tenemos las opciones para cada item del menu.
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                Toast.makeText(this, "Hago click en boton login", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_registro:
+                Toast.makeText(this, "Haglo click en el boton registro", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_login_oculto:
+                Toast.makeText(this, "Hago click en boton login oculto", Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.action_registro_oculto:
+                Toast.makeText(this, "Haglo click en el boton registro oculto", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                //Aqui la accion del usuario no fue reconocida
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+    /*
     @SuppressLint("SetTextI18n")
     @Override
     public void dataChangeToolbar(String titulo, String usuario, String idUsuario, boolean verBotones) {
         super.dataChangeToolbar(titulo, usuario, idUsuario, verBotones);
-    }
+    }*/
 
     private boolean verificarConexion() {
         ConnectivityManager connectivityManager =
