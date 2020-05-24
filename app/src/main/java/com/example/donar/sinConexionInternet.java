055c4faea70f71371e2a53866bf5e0a6ab9bcc57
niru1990 <nicolas.reyes90@gmail.com>
2020-05-24 12:43:24 -0300

    Se agrega layout para cuando no hay conexion Se modifica mainactivity para realizar el llamado a este layout en caso de no haber conexion.

---
+++ app/src/main/java/com/example/donar/sinConexionInternet.java
@@ -0,0 +1,65 @@
+package com.example.donar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class sinConexionInternet extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sin_conexion_internet);
+        toolbar = (Toolbar) findViewById(R.id.donArToolBar);
+        setSupportActionBar(toolbar);
+    }
+
+    /**
+     * Aqui creamos las opciones de Menu con el toolbar.
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    /**
+     * Aqui tenemos las opciones para cada item del menu.
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_login:
+                Toast.makeText(this, "Hago click en boton login", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_registro:
+                Toast.makeText(this, "Haglo click en el boton registro", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_login_oculto:
+                Toast.makeText(this, "Hago click en boton login oculto", Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.action_registro_oculto:
+                Toast.makeText(this, "Haglo click en el boton registro oculto", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                //Aqui la accion del usuario no fue reconocida
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
