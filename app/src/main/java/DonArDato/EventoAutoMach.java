9126e0ec14edce805ab081578afa278df32f8240
Nico <nicolas.reyes90@gmail.com>
2020-06-03 20:41:35 -0300

    Refactor de nombre de parametros del constructor

--- app/src/main/java/DonArDato/EventoAutoMach.java
+++ app/src/main/java/DonArDato/EventoAutoMach.java
@@ -8,18 +8,14 @@ public class EventoAutoMach {
     private String nombre;
     private String apellido;
 
-    public EventoAutoMach(String idEvento, String nombre, String apellido){
-        this.nombre = nombre;
+    public EventoAutoMach(String idEvento, String nombrePaciente, String apellidoPaciente){
         this.idEvento = idEvento;
-        this.apellido = apellido;
+        this.nombre = nombrePaciente;
+        this.apellido = apellidoPaciente;
     }
 
     public String getIdEvento(){return  this.idEvento;}
     public String getNombre(){return this.nombre;}
     public String getApellido(){return this.apellido;}
-
-    public void setIdEvento(String idEvento){this.idEvento = idEvento;}
-    public void setNombre(String nombre){this.nombre = nombre;}
-    public void setApellido(String apellido){this.apellido = apellido;}
 
 }
