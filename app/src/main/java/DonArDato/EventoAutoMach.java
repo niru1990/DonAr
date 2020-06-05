98a99592de74b7a3ec9ec91249e11bbf450da89d
Nico <nicolas.reyes90@gmail.com>
2020-06-05 20:32:17 -0300

    se agrega la propiedad de nombre del medico

--- app/src/main/java/DonArDato/EventoAutoMach.java
+++ app/src/main/java/DonArDato/EventoAutoMach.java
@@ -7,15 +7,21 @@ public class EventoAutoMach {
     private String idEvento;
     private String nombre;
     private String apellido;
+    private String nombreMedico;
 
-    public EventoAutoMach(String idEvento, String nombrePaciente, String apellidoPaciente){
+    public EventoAutoMach(String idEvento, String nombrePaciente, String apellidoPaciente, String nombreMedico){
         this.idEvento = idEvento;
         this.nombre = nombrePaciente;
         this.apellido = apellidoPaciente;
+        if(nombreMedico == null)
+            this.nombreMedico = "";
+        else
+            this.nombreMedico = nombreMedico;
     }
 
     public String getIdEvento(){return  this.idEvento;}
     public String getNombre(){return this.nombre;}
     public String getApellido(){return this.apellido;}
+    public String getNombreMedico(){return this.nombreMedico;}
 
 }
