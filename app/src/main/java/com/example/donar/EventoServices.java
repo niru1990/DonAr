f23fc929b2bcd11d9de17bbc6c9dc405ddf12ea8
Nico <nicolas.reyes90@gmail.com>
2020-06-05 20:34:50 -0300

    se agregan config de llamadas a endpoints

--- app/src/main/java/com/example/donar/EventoServices.java
+++ app/src/main/java/com/example/donar/EventoServices.java
@@ -1,17 +1,20 @@
 
 package com.example.donar;
 
+import com.google.common.reflect.Parameter;
+
 import java.math.BigInteger;
 import java.util.List;
 
+import DonArDato.AsignarEspecialidadDTO;
 import DonArDato.EventoDTO;
-import DonArDato.EventoRedicidoDTO;
-import DonArDato.PacienteDTO;
+import DonArDato.EventoReducidoDTO;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -24,11 +27,11 @@ public interface EventoServices {
     Call<Void> addEvento(@Body EventoDTO evento);
 
     //Actualizar eventos
-    String API_ROUTE_UPDATE= "api/update/{id}";
+    String API_ROUTE_UPDATE= "api/evento/asignarEspecialidad/";
+    @Headers("Content-Type: application/json")
     @PUT(API_ROUTE_UPDATE)
-    @FormUrlEncoded
-    Call<Void> updateEvento(@Path("evento") BigInteger id,
-                          @Field("idEspecialidad") Integer idEspecialidad);
+    //@FormUrlEncoded
+    Call<EventoDTO> updateEvento(@Body AsignarEspecialidadDTO actualizador);
 
     //Obtener evento por id de voluntario
     String API_ROUTE_GET_BY_VoluntarioID = "api/evento/eventosporvoluntario/{id}";
@@ -38,7 +41,7 @@ public interface EventoServices {
     //Obtengo evento reducido
     String API_ROUTE_GET_EVENT_REDUCED_BY_ID = "api/evento/eventoDTO/{id}";
     @GET(API_ROUTE_GET_EVENT_REDUCED_BY_ID)
-    Call<EventoRedicidoDTO> getEventoReducidoById(@Path("id") String id);
+    Call<EventoReducidoDTO> getEventoReducidoById(@Path("id") String id);
 
     //Obtener evento por id
     String API_ROUTE_GET_BY_ID = "api/evento/{id}";
