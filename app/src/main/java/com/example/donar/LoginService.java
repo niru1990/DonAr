93bdb1f058ae1af1cfbaccc77d403667403705ed
Nico <nicolas.reyes90@gmail.com>
2020-06-11 23:57:50 -0300

    Se agrega llamada a endpoint como guia

--- app/src/main/java/com/example/donar/LoginService.java
+++ app/src/main/java/com/example/donar/LoginService.java
@@ -1,10 +1,15 @@
 package com.example.donar;
 
 import Negocio.Login;
+
+import DonArDato.actualizaIG;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
@@ -19,5 +24,10 @@ public interface LoginService {
             @Field("IdGoogle") String IdGoogle
     );
 
+    //Editar con Objeto
+    String API_ROUTE_UPDATE_DATA = "api/Login/";
+    @Headers("Content-Type: application/json")
+    @PUT(API_ROUTE_UPDATE_DATA)
+    Call<Login> updateData(@Body actualizaIG aic);
 
 }
